c5d99a4b1630cf1c03066c3ac7b239b921e11f82
phatphoom <99587354+phatphoom@users.noreply.github.com>
2023-12-12 06:16:47 +0700

    add bootstrap

--- login-register/index.php
+++ login-register/index.php
@@ -1,14 +1,25 @@
-<?php session_start()?>
+<?php
+ session_start();
+ 
+ include "db.php";
+ if(!isset($_SESSION['email'])){
+  header("localtion:learn.php");
+ }
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <h2>Hello</h2>
-    <a href="login.php">login</a>
-    <a href="logout.php">logout</a>
+    <h3>Wellcome</h3>
+
+    <!-- <a class="btn btn-primary" href="login.php" role="button">login</a> -->
+    <a class="btn btn-danger" href="logout.php" role="button">logout</a>
 </body>
 </html>
--- login-register/login.php
+++ login-register/login.php
@@ -5,15 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <style>
+        .content {
+            width: 400px;
+        }
+    </style>
 </head>
 <body>
-    <h2>เข้าสู่ระบบ</h2>
-    <form action="login_funch.php" method="post">
-        <input type="text" name="email" placeholder="email"><br>
-        <input type="password" name="password" placeholder="password"><br>
-        <input type="submit" value="เข้าสู่ระบบ">
-        <p>not have user yet <a href="register.php">สมัครสมาชิก</a></p>
+    <h2 class="text-center mt-5">เข้าสู่ระบบ</h2>
+   
+    <div class="container-md mt-5 content">
+        <form action="login_funch.php" method="post">
+            <input type="text" name="email" placeholder="email" 
+            class="form-control"><br>
+            <input type="password" name="password" placeholder="password" class="form-control"><br>
+            <input type="submit" value="สมัครสมาชิก" class="btn btn-primary mt-2">
+            <p>not have user yet<a href="register.php"> สมัครสมาชิก</a></p>
+        </form>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-    </form>
 </body>
 </html>
